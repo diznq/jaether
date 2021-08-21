44321659a93aa81582d9e6ff5aa95c76d93b26b0
diznq <13458283+diznq@users.noreply.github.com>
2021-08-21 21:17:59 +0200

    add missing opcodes except invokes

---
+++ jaether/Assets/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args){
+        int i = 0;
+        for(int j = 0; j < 50; j++){
+            i += j;
+        }
+        System.out.println(i);
+    }
+}
--- jaether/CPU.cpp
+++ jaether/CPU.cpp
@@ -46,20 +46,36 @@ public:
 };
 
 class vCPU {
+	bool _running = true;
 public:
 	vCPU() {
 		// ...
 	}
 
-	size_t execute(const V<vFrame>& frame) {
-		size_t step = sub_execute(frame);
+	bool active() const {
+		return _running;
+	}
+
+	vCOMMON run(const  V<vFrame>& frame) {
+		_running = true;
+		vCOMMON retVal;
+		memset(&retVal, 0, sizeof(retVal));
+		while (active()) {
+			execute(frame, retVal);
+		}
+		return retVal;
+	}
+
+	size_t execute(const V<vFrame>& frame, vCOMMON& retVal) {
+		size_t step = sub_execute(frame, retVal);
 		frame->incrpc(step + 1);
 		return step;
 	}
 
-	size_t sub_execute(const V<vFrame>& frame) {
+	size_t sub_execute(const V<vFrame>& frame, vCOMMON& retVal) {
 		V<vStack>& _stack = frame->_stack;
 		V<vMemory>& _local = frame->_local;
+		V<vMemory>& _constPool = frame->_constPool;
 		vBYTE* ip = frame->fetch();
 		vBYTE& opcode = *ip;
 		vCOMMON op[8];
@@ -112,13 +128,65 @@ public:
 			_stack->push<vBYTE>(read<vBYTE>(ip + 1));
 			return 1;
 		case sipush:
-			_stack->push<vSHORT>(read<vSHORT>(ip + 1));
+			op[0].usi = read<vBYTE>(ip + 1);
+			op[0].usi <<= 8;
+			op[0].usi |= read<vBYTE>(ip + 2);
+			_stack->push<vUSHORT>(op[0].usi);
 			return 2;
 		case dup:
 			op[0] = _stack->pop<vCOMMON>();
 			_stack->push(op[0]);
 			_stack->push(op[0]);
 			return 0;
+
+		case i2b:
+			_stack->push<vBYTE>((vBYTE)(_stack->pop<vUINT>() & 255));
+			break;
+		case i2c:
+			_stack->push<vJCHAR>((vJCHAR)(_stack->pop<vUINT>() & 65535));
+			break;
+		case i2s:
+			_stack->push<vUSHORT>((vUSHORT)(_stack->pop<vUINT>() & 65535));
+			break;
+		case i2l:
+			_stack->push<vLONG>((vLONG)(_stack->pop<vINT>()));
+			break;
+		case i2f:
+			_stack->push<vFLOAT>((vFLOAT)(_stack->pop<vINT>()));
+			break;
+		case i2d:
+			_stack->push<vDOUBLE>((vDOUBLE)(_stack->pop<vINT>()));
+			break;
+
+		case l2i:
+			_stack->push<vINT>((vINT)_stack->pop<vLONG>());
+			break;
+		case l2f:
+			_stack->push<vFLOAT>((vFLOAT)_stack->pop<vLONG>());
+			break;
+		case l2d:
+			_stack->push<vDOUBLE>((vDOUBLE)_stack->pop<vLONG>());
+			break;
+
+		case f2d:
+			_stack->push<vDOUBLE>((vDOUBLE)_stack->pop<vFLOAT>());
+			break;
+		case f2i:
+			_stack->push<vINT>((vINT)_stack->pop<vFLOAT>());
+			break;
+		case f2l:
+			_stack->push<vLONG>((vLONG)_stack->pop<vFLOAT>());
+			break;
+
+		case d2f:
+			_stack->push<vFLOAT>((vFLOAT)_stack->pop<vDOUBLE>());
+			break;
+		case d2i:
+			_stack->push<vINT>((vINT)_stack->pop<vDOUBLE>());
+			break;
+		case d2l:
+			_stack->push<vLONG>((vLONG)_stack->pop<vDOUBLE>());
+			break;
 
 		case ineg:
 			op[0].i = _stack->pop<vINT>();
@@ -200,6 +268,28 @@ public:
 			_stack->push<vDOUBLE>(op[0].d / op[1].d);
 			return 0;
 
+		case imul:
+			op[1].i = _stack->pop<vINT>();
+			op[0].i = _stack->pop<vINT>();
+			printf("imul %d, %d\n", op[1].i, op[0].i);
+			_stack->push<vINT>(op[0].i * op[1].i);
+			return 0;
+		case lmul:
+			op[1].l = _stack->pop<vLONG>();
+			op[0].l = _stack->pop<vLONG>();
+			_stack->push<vLONG>(op[0].l * op[1].l);
+			return 0;
+		case fmul:
+			op[1].f = _stack->pop<vFLOAT>();
+			op[0].f = _stack->pop<vFLOAT>();
+			_stack->push<vFLOAT>(op[0].f * op[1].f);
+			return 0;
+		case dmul:
+			op[1].d = _stack->pop<vDOUBLE>();
+			op[0].d = _stack->pop<vDOUBLE>();
+			_stack->push<vDOUBLE>(op[0].d * op[1].d);
+			return 0;
+
 		case irem:
 			op[1].i = _stack->pop<vINT>();
 			op[0].i = _stack->pop<vINT>();
@@ -219,11 +309,6 @@ public:
 			op[1].d = _stack->pop<vDOUBLE>();
 			op[0].d = _stack->pop<vDOUBLE>();
 			_stack->push<vDOUBLE>(std::fmod(op[0].d, op[1].d));
-			return 0;
-
-		case iinc:
-			op[0].i = _stack->pop<vINT>();
-			_stack->push<vINT>(op[0].i + 1);
 			return 0;
 
 		case ior:
@@ -466,6 +551,167 @@ public:
 		case dstore_3:
 			_local->set<vDOUBLE>((size_t)3, _stack->pop<vDOUBLE>());
 			return 0;
+
+		case ldc:
+			op[0].b = read<vBYTE>(ip + 1);
+			_stack->push<vCOMMON>(_constPool->get<vCOMMON>((size_t)op[0].b));
+			return 1;
+		case ldc_w:
+		case ldc2_w:
+			op[0].usi = readUSI(ip + 1);
+			_stack->push<vCOMMON>(_constPool->get<vCOMMON>((size_t)op[0].usi));
+			return 2;
+
+		case iinc:
+			op[0].b = read<vBYTE>(ip + 1);
+			op[1].b = read<vBYTE>(ip + 2);
+			_local->set<vINT>(op[0].b, _local->get<vINT>(op[0].b) + op[1].b);
+			return 2;
+
+		case goto_:
+			op[0].usi = readUSI(ip + 1);
+			return ((size_t)(op[0].si)) - 1;
+		case goto_w:
+			op[0].u = readUI(ip + 1);
+			return ((size_t)(op[0].i)) - 1;
+		case ifeq:
+			op[0].usi = readUSI(ip + 1);
+			op[1].i = _stack->pop<vINT>();
+			if (op[1].i == 0)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+		case ifne:
+			op[0].usi = readUSI(ip + 1);
+			op[1].i = _stack->pop<vINT>();
+			if (op[1].i != 0)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+		case ifnull:
+			op[0].usi = readUSI(ip + 1);
+			op[1].a = _stack->pop<vREF>();
+			if (op[1].a == 0)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+		case ifnonnull:
+			op[0].usi = readUSI(ip + 1);
+			op[1].a = _stack->pop<vREF>();
+			if (op[1].a != 0)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+
+		case iflt:
+			op[0].usi = readUSI(ip + 1);
+			op[1].i = _stack->pop<vINT>();
+			if (op[1].i < 0)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+		case ifgt:
+			op[0].usi = readUSI(ip + 1);
+			op[1].i = _stack->pop<vINT>();
+			if (op[1].i > 0)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+		case ifle:
+			op[0].usi = readUSI(ip + 1);
+			op[1].i = _stack->pop<vINT>();
+			if (op[1].i <= 0)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+		case ifge:
+			op[0].usi = readUSI(ip + 1);
+			op[1].i = _stack->pop<vINT>();
+			if (op[1].i >= 0)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+
+		case if_icmpeq:
+			op[0].usi = readUSI(ip + 1);
+			op[2].i = _stack->pop<vINT>();
+			op[1].i = _stack->pop<vINT>();
+			if (op[1].i == op[1].i)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+		case if_icmpne:
+			op[0].usi = readUSI(ip + 1);
+			op[2].i = _stack->pop<vINT>();
+			op[1].i = _stack->pop<vINT>();
+			if (op[1].i != op[2].i)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+		case if_icmplt:
+			op[0].usi = readUSI(ip + 1);
+			op[2].i = _stack->pop<vINT>();
+			op[1].i = _stack->pop<vINT>();
+			if (op[1].i < op[2].i)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+		case if_icmpgt:
+			op[0].usi = readUSI(ip + 1);
+			op[2].i = _stack->pop<vINT>();
+			op[1].i = _stack->pop<vINT>();
+			if (op[1].i > op[2].i)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+		case if_icmple:
+			op[0].usi = readUSI(ip + 1);
+			op[2].i = _stack->pop<vINT>();
+			op[1].i = _stack->pop<vINT>();
+			if (op[1].i <= op[2].i)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+		case if_icmpge:
+			op[0].usi = readUSI(ip + 1);
+			op[2].i = _stack->pop<vINT>();
+			op[1].i = _stack->pop<vINT>();
+			if (op[1].i >= op[2].i)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+		case if_acmpeq:
+			op[0].usi = readUSI(ip + 1);
+			op[2].a = _stack->pop<vREF>();
+			op[1].a = _stack->pop<vREF>();
+			if (op[1].a == op[2].a)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+		case if_acmpne:
+			op[0].usi = readUSI(ip + 1);
+			op[2].a = _stack->pop<vREF>();
+			op[1].a = _stack->pop<vREF>();
+			if (op[1].a != op[2].a)
+				return ((size_t)(op[0].si)) - 1;
+			else return 2;
+
+		case lcmp:
+			op[1].l = _stack->pop<vLONG>();
+			op[0].l = _stack->pop<vLONG>();
+			_stack->push<vINT>(compare(op[0].l, op[0].l));
+			return 0;
+		case fcmpg:
+		case fcmpl:
+			op[1].f = _stack->pop<vFLOAT>();
+			op[0].f = _stack->pop<vFLOAT>();
+			_stack->push<vINT>(compare(op[0].f, op[0].f));
+			return 0;
+		case dcmpg:
+		case dcmpl:
+			op[1].d = _stack->pop<vDOUBLE>();
+			op[0].d = _stack->pop<vDOUBLE>();
+			_stack->push<vINT>(compare(op[0].d, op[0].d));
+			return 0;
+
+		case return_:
+			_running = false;
+			return 0;
+
+		case ireturn:
+		case lreturn:
+		case dreturn:
+		case freturn:
+		case areturn:
+			_running = false;
+			retVal = _stack->pop<vCOMMON>();
+			return 0;
+
 		default:
 			return 0;
 		}
@@ -474,24 +720,61 @@ public:
 	template<class T> T read(vBYTE* ip) const {
 		return *(T*)ip;
 	}
+
+	vUSHORT readUSI(vBYTE* ip) const {
+		vUSHORT usi = read<vBYTE>(ip);
+		usi <<= 8;
+		usi |= read<vBYTE>(ip + 1);
+		return usi;
+	}
+
+	vUINT readUI(vBYTE* ip) const {
+		vUINT ui = read<vBYTE>(ip);
+		ui <<= 8; ui |= read<vBYTE>(ip + 1);
+		ui <<= 8; ui |= read<vBYTE>(ip + 2);
+		ui <<= 8; ui |= read<vBYTE>(ip + 3);
+		return ui;
+	}
+
+	template<class T> vINT compare(T a, T b) {
+		if (a == b) return 0;
+		if (a > b) return 1;
+		return -1;
+	}
 };
 
 int main() {
 	auto cpu = VMAKE(vCPU);
 	auto stk = VMAKE(vStack);
 	vBYTE bytecode[] = {
-		nop,
-		nop,
-		iconst_2,
+		iconst_m1,
+		
 		istore,
-		5,
+		0,
+
+		iinc,
+		0,
+		1,
+
+		iload,
+		0,
+
 		iconst_5,
-		istore,
-		5
+
+		if_icmplt,
+		0xFF,
+		0xFA,
+
+		iload,
+		0,
+
+		iload,
+		0,
+
+		imul,
+		ireturn
 	};
 	auto frame = VMAKE(vFrame, V<vBYTE>(bytecode));
-	for (int i = 0; i < 6; i++) {
-		cpu->execute(frame);
-		printf("%lld\n", frame->_local->get<vLONG>(5));
-	}
+	vCOMMON retval = cpu->run(frame);
+	printf("%d\n", retval.i);
 }
