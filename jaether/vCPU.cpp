2270cb35c1c1a8398414d939f7ae7cab9acab7c3
diznq <13458283+diznq@users.noreply.github.com>
2021-08-21 13:11:57 +0200

    add opcodes

--- jaether/vCPU.cpp
+++ jaether/vCPU.cpp
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <assert.h>
 
+#include "Opcodes.h"
+
 typedef int8_t		vCHAR;
 typedef int16_t		vSHORT;
 typedef int32_t		vINT;
@@ -11,6 +13,8 @@ typedef uint8_t		vBYTE;
 typedef uint16_t	vUSHORT;
 typedef uint32_t	vUINT;
 typedef uint64_t	vULONG;
+typedef uint32_t	vCAT1;
+typedef uint64_t	vCAT2;
 typedef float vFLOAT;
 typedef double vDOUBLE;
 
@@ -87,8 +91,71 @@ public:
 	}
 };
 
+template<size_t N = 1024>
 class vCPU {
+	vBYTE* _program;
+	V<vStack<N>> _stack;
+public:
+	vCPU() {
+		_stack = VMAKE(vStack<N>);
+	}
+	size_t execute(vBYTE* ip) {
+		vBYTE opcode = ip[0];
+		switch (opcode) {
+		case iconst_0:
+			_stack->push<vCAT1>(0);
+			return 0;
+		case iconst_1:
+			_stack->push<vCAT1>(1);
+			return 0;
+		case iconst_2:
+			_stack->push<vCAT1>(2);
+			return 0;
+		case iconst_3:
+			_stack->push<vCAT1>(3);
+			return 0;
+		case iconst_4:
+			_stack->push<vCAT1>(4);
+			return 0;
+		case iconst_5:
+			_stack->push<vCAT1>(5);
+			return 0;
+		case iconst_m1:
+			_stack->push<vCAT1>(-1);
+			return 0;
+		case lconst_0:
+			_stack->push<vCAT2>(0);
+			return 0;
+		case lconst_1:
+			_stack->push<vCAT2>(1);
+			return 0;
+		case dconst_0:
+			_stack->push<double>(0.0);
+			return 0;
+		case dconst_1:
+			_stack->push<double>(1.0);
+			return 0;
+		case fconst_0:
+			_stack->push<float>(0.0f);
+			return 0;
+		case fconst_1:
+			_stack->push<float>(1.0f);
+			return 0;
+		case fconst_2:
+			_stack->push<float>(2.0f);
+			return 0;
+		case bipush:
+			_stack->push<vCAT1>(read<vBYTE>(ip + 1));
+			return 1;
+		case sipush:
+			_stack->push<vCAT1>(read<vSHORT>(ip + 1));
+			return 2;
+		}
+	}
 
+	template<class T> T read(vBYTE* ip) const {
+		return *(T*)ip;
+	}
 };
 
 int main() {
